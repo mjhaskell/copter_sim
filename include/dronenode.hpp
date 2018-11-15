e906fdc6c514357eeadf1c436cba94e857c273e5
Mat Haskell <mhaskell9@gmail.com>
2018-11-14 18:45:37 -0700

    Set up ros node for drone dynamics

---
+++ include/dronenode.hpp
@@ -0,0 +1,70 @@
+#ifndef DRONENODE_HPP
+#define DRONENODE_HPP
+
+#include <ros/ros.h>
+#include <QThread>
+#include "nav_msgs/Odometry.h"
+#include "rosflight_msgs/Command.h"
+
+namespace quad
+{
+typedef struct
+{
+  double pn;
+  double pe;
+  double pd;
+
+  double phi;
+  double theta;
+  double psi;
+
+  double u;
+  double v;
+  double w;
+
+  double p;
+  double q;
+  double r;
+}state_t;
+
+typedef struct
+{
+    double u1;
+    double u2;
+    double u3;
+    double u4;
+
+    double F;
+    double phi_c;
+    double theta_c;
+    double r_c;
+}input_t;
+
+class DroneNode : public QThread
+{
+    Q_OBJECT
+public:
+    DroneNode(int argc, char** argv);
+    virtual ~DroneNode();
+    bool init();
+    bool init(const std::string &master_url,const std::string &host_url);
+    void run();
+
+signals:
+    void statesChanged(nav_msgs::Odometry* odom);
+
+private:
+    int m_argc;
+    char** m_argv;
+    state_t m_x;
+    input_t m_u;
+    uint8_t m_control_mode;
+    nav_msgs::Odometry m_odom;
+    ros::Publisher m_state_pub;
+    ros::Subscriber m_cmd_sub;
+    void updateDynamics();
+    void cmdCallback(const rosflight_msgs::CommandConstPtr &msg);
+};
+
+} // end namespace quad
+#endif // DRONENODE_HPP
---
+++ src/dronenode.cpp
@@ -0,0 +1,105 @@
+#include "dronenode.hpp"
+#include <string>
+#include "nav_msgs/Odometry.h"
+#include "rosflight_msgs/Command.h"
+
+namespace quad
+{
+
+DroneNode::DroneNode(int argc, char **argv) :
+    m_argc{argc},
+    m_argv{argv}
+{
+    m_odom.pose.pose.position.x = 0;
+    m_odom.pose.pose.position.y = 0;
+    m_odom.pose.pose.position.z = 0;
+    m_odom.pose.pose.orientation.w = 1;
+    m_odom.pose.pose.orientation.x = 0;
+    m_odom.pose.pose.orientation.y = 0;
+    m_odom.pose.pose.orientation.z = 0;
+}
+
+DroneNode::~DroneNode()
+{
+    if(ros::isStarted())
+    {
+        ros::shutdown();
+        ros::waitForShutdown();
+    }
+    wait();
+}
+
+bool DroneNode::init()
+{
+    ros::init(m_argc,m_argv,"flight_controller_node");
+    if (!ros::master::check())
+        return false;
+    ros::start();
+    ros::NodeHandle nh;
+    m_state_pub = nh.advertise<nav_msgs::Odometry>("states", 25);
+    m_cmd_sub = nh.subscribe("commands", 50, &DroneNode::cmdCallback,this);
+    //    volatile FlightControllerROS fc_ros;
+    start();
+    return true;
+}
+
+bool DroneNode::init(const std::string &master_url, const std::string &host_url)
+{
+    std::map<std::string,std::string> remappings;
+    remappings["__master"] = master_url;
+    remappings["__hostname"] = host_url;
+    ros::init(remappings,"qt_test");
+    if ( ! ros::master::check() ) {
+        return false;
+    }
+    ros::start();
+    ros::NodeHandle nh;
+    m_state_pub = nh.advertise<nav_msgs::Odometry>("states", 25);
+    m_cmd_sub = nh.subscribe("commands", 50, &DroneNode::cmdCallback,this);
+    start();
+    return true;
+}
+
+void DroneNode::run()
+{
+    ros::Rate publish_rate(30);
+    while (ros::ok())
+    {
+        this->updateDynamics();
+        m_state_pub.publish(m_odom);
+        ros::spinOnce();
+        publish_rate.sleep();
+    }
+}
+
+void DroneNode::updateDynamics()
+{
+    if (m_odom.pose.pose.position.z > -3)
+        m_odom.pose.pose.position.z -= 0.01;
+    emit statesChanged(&m_odom);
+}
+
+void DroneNode::cmdCallback(const rosflight_msgs::CommandConstPtr &msg)
+{
+    m_control_mode = msg->mode;
+    switch(msg->mode)
+    {
+    case rosflight_msgs::Command::MODE_PASS_THROUGH:
+        m_u.u1 = msg->x;
+        m_u.u2 = msg->y;
+        m_u.u3 = msg->z;
+        m_u.u4 = msg->F;
+        break;
+    case rosflight_msgs::Command::MODE_ROLL_PITCH_YAWRATE_ALTITUDE:
+        m_u.phi_c = msg->x;
+        m_u.theta_c = msg->y;
+        m_u.r_c = msg->z;
+        m_u.F = msg->F;
+        break;
+    default:
+        ROS_ERROR("FlightControllerNode: Unhandled command message of type %d",msg->mode);
+        break;
+    }
+}
+
+} // end namespace fc
